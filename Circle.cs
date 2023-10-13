895890a526394d65813278959f088973b9df8a57
Morgan <144250478+IceDropp@users.noreply.github.com>
2023-10-13 09:40:33 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb5MorganWestinOop
+{
+    public class Circle
+    {
+        private float radius;
+
+        public Circle(float radius)
+        {
+            this.radius = radius;
+        }
+
+        public float GetArea()
+        {
+            return MathF.PI * radius * radius;
+        }
+
+        public float GetCircumference()
+        {
+            return 2 * MathF.PI * radius;
+        }
+
+        public float GetVolumeAsSphere()
+        {
+            // Anta att detta är en sfär och beräkna volymen.
+            return (4.0f / 3.0f) * MathF.PI * MathF.Pow(radius, 3);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Labb5MorganWestinOop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            float area1 = circle1.GetArea();
+            float area2 = circle2.GetArea();
+
+            float circumference1 = circle1.GetCircumference();
+            float circumference2 = circle2.GetCircumference();
+
+            float volume1 = circle1.GetVolumeAsSphere();
+            float volume2 = circle2.GetVolumeAsSphere();
+
+            Console.WriteLine("Area of circle with radius 5: " + area1);
+            Console.WriteLine("Area of circle with radius 6: " + area2);
+
+            Console.WriteLine("Circumference of circle with radius 5: " + circumference1);
+            Console.WriteLine("Circumference of circle with radius 6: " + circumference2);
+
+            Console.WriteLine("Volume of sphere with radius 5 (as if it were a sphere): " + volume1);
+            Console.WriteLine("Volume of sphere with radius 6 (as if it were a sphere): " + volume2);
+
+            Console.ReadLine();
+        }   
+    }
+}
